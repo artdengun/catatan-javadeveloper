0d0951dcad944681d996fda2e712ce339b18ab42
Deni Gunawan <artdengun@gmail.com>
2020-10-20 22:21:22 +0700

    catatan-javadeveloper

---
+++ src/main/java/com/deni/catatanjavaDeveloper/CatatanJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.deni.catatanjavaDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatatanJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatatanJavaDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/catatanjavaDeveloper/CatatanJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.catatanjavaDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatatanJavaDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
